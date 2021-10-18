cb9a342c8e063c26909e66b80b7182a60b32b051
mashico1001 <ec2-user@ip-172-31-34-79.ec2.internal>
2021-10-18 11:33:30 +0000

    [Update]3-9

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_17_220605) do
+ActiveRecord::Schema.define(version: 2021_10_18_083803) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.string "category"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
