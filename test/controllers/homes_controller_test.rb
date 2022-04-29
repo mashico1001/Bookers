896e3344c6240d1f872d5e4bbf2c9636522bbf6a
mashico1001 <wabumpr@gmail.com>
2022-04-29 13:53:29 +0000

    [Add]test作成

--- test/controllers/homes_controller_test.rb
+++ test/controllers/homes_controller_test.rb
@@ -4,4 +4,11 @@ class HomesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "should get top" do
+    get root_url
+    assert_response :success
+  end
+
+
 end
